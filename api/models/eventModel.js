908f8dd13dae5a5eb80d164c8fb2678e06391980
root <alexballe35@gmail.com>
2020-09-23 02:13:01 +0200

    find one with photo

--- api/models/eventModel.js
+++ api/models/eventModel.js
@@ -74,10 +74,12 @@ Event.findById = (eventId, result) => {
     }
 
     connection.query(
-        `SELECT _assoc.name, _assoc.website, _assoc.email, _assoc.landline, _assoc.facebook, _assoc_address.street, _assoc.twitter, _e.title, _e.description, _e.start_date, _e.end_date, _e.publish_date, _e.rating
+        `SELECT _assoc.name, _assoc.website, _assoc.email, _assoc.photo, _assoc.landline, _assoc.facebook, _assoc_address.street, _assoc.twitter, GROUP_CONCAT(_ec.label) AS categories, _e.title, _e.description, _e.start_date, _e.end_date, _e.publish_date, _e.rating
         FROM event _e
         INNER JOIN user _assoc ON _assoc.id = _e.owner_id
         LEFT JOIN address _assoc_address ON _assoc.id = _assoc_address.user_id
+        LEFT JOIN event_category_event _ece ON _e.id = _ece.event_id
+        LEFT JOIN event_category _ec ON _ec.id = _ece.event_category_id
         WHERE _e.id = ?`,
       eventId,
       (err, res) => {
